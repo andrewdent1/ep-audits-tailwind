57dd218e250c1224c03bb92d81fe6d53a523fc35
Andrew Dent <dentdrew@gmail.com>
2024-11-25 22:46:42 -0500

    homepage styling

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust based on your folder structure
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
